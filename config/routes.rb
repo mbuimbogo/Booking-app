1488e248d45ee987eb49a40d4fecb9ee0b907682
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-12-03 20:06:52 +0300

    Create booking, user, review and restaurant resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :bookings
+  resources :reviews
+  resources :users
+  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
