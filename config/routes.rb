99a22bea6fded13bf144d5e93b44f9db91394afb
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-12-05 00:35:09 +0300

    Add a SessionsController#destroy method and log out route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   patch '/restaurants/:id/like', to: 'restaurants#increment_likes'
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
